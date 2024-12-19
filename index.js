56afce345c7b0ec9acfe9aabae761c6d17f0ee18
Sincan Maulana <sincanmaulanaa@gmail.com>
2024-12-19 13:58:52 +0000

    Implement default endpoint

--- index.js
+++ index.js
@@ -23,6 +23,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api', function (req, res) {
+  const now = new Date();
+  res.json({
+    unix: now.getTime(),
+    utc: now.toUTCString()
+  })
+})
+
 app.get('/api/:date', (req, res) => {
   const { date } = req.params;
 
